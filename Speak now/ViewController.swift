3e198afbb80fba80fdc812cd0d876701b02c341d
Nha T.Tran <nhat.tran@Nhas-Mac.local>
2017-05-18 20:02:19 -0700

    Initial Commit

---
+++ Speak now/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speak now
+//
+//  Created by Nha T.Tran on 5/18/17.
+//  Copyright © 2017 Nha T.Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
